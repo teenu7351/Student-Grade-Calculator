be7e28097f024eb7eb6135d8095cae6ce4c10dcc
Teenu Anand <126483430+teenu7351@users.noreply.github.com>
2023-09-16 20:39:41 +0530

    Update StudentGradeCalculator.java

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -4,15 +4,11 @@ public class StudentGradeCalculator
 {
     public static void main(String args[])
     {
-    	/* This program assumes that the student has 6 subjects,
-    	 * thats why I have created the array of size 6. You can
-    	 * change this as per the requirement.
-    	 */
-        int marks[] = new int[6];
+        int marks[] = new int[5];
         int i;
         float total=0, avg;
         try (Scanner scanner = new Scanner(System.in)) {
-            for(i=0; i<6; i++) { 
+            for(i=0; i<5; i++) { 
                System.out.print("Enter Marks of Subject"+(i+1)+":");
                marks[i] = scanner.nextInt();
                total = total + marks[i];
@@ -22,7 +18,7 @@ public class StudentGradeCalculator
 		
         
         //Calculating average here
-        avg = total/6;
+        avg = total/5;
         System.out.print("The student Grade is: ");
         if(avg>=80)
         {
